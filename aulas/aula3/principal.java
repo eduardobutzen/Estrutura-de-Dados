ce8bacb2c44ee7290b6f8b9e127cdf2eb3b8ee84
eduardobutzen <eduardo.butzen@ufn.edu.br>
2026-03-11 08:39:01 -0300

    aula3

--- aulas/aula3/principal.java
+++ aulas/aula3/principal.java
@@ -10,27 +10,39 @@ public class principal {
         Scanner teclado = new Scanner(System.in);
         
         ArrayList<glicemia> lista = new ArrayList<>();
+
+        
        
 
         //alternativa 1
          glicemia obj = new glicemia(191, "11/03/2026", "08:00");
          lista.add(obj);
 
-         //alternativa 2
-         lista.add (new glicemia(191, "11/03/2026", "08:00"));
+        //alternativa 2
+        lista.add (new glicemia(191, "11/03/2026", "08:00"));
 
         //alternativa 3
         int valor;
         String data, hora;
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.print("Digite o valor da glicemia: ");
             valor = teclado.nextInt();
-            System.out.print("Digite a data: ");
-            data = teclado.next();
-            System.out.print("Digite a hora: ");
-            hora = teclado.next();
+            teclado.nextLine(); 
+            System.out.print("Digite a data: [dd/mm/aaaa] ");
+            data = teclado.nextLine();
+            System.out.print("Digite a hora: [hh:mm] ");
+            hora = teclado.nextLine();
             lista.add(new glicemia(valor, data, hora));
         }
+    
+        //exibit lista - alternativa 1
+        for(int i = 0; i < lista.size(); i++) {
+        
+            System.out.println(lista.get(i).valor + " - " + lista.get(i).data + " - " + lista.get(i).hora);
+
+        }   
+
+
 
         teclado.close();
     }
